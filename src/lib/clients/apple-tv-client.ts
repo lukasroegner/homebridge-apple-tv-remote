a1a1952320dadb48faa43123d8d5d31041a1e438
Lukas Rögner <lukas.roegner@MacBook-Pro.lan>
2020-04-30 13:28:10 +0200

    Enhanced the check for on/off state.

--- src/lib/clients/apple-tv-client.ts
+++ src/lib/clients/apple-tv-client.ts
@@ -60,6 +60,25 @@ export class AppleTvClient extends EventEmitter {
     }
 
     /**
+     * Tries to get the on/off state of the Apple TV
+     * @param messagePayload The message payload that contains the information to check whether the Apple TV is on.
+     * @returns Returns a value that determines whether the Apple TV is on.
+     */
+    private getIsOn(messagePayload: any): boolean {
+
+        // If the Apple TV is not a proxy for AirPlay playback, the logicalDeviceCount determines the state
+        if (messagePayload.logicalDeviceCount > 0 && !messagePayload.isProxyGroupPlayer) {
+            return true;
+        }
+
+        // If the Apple TV is a proxy for AirPlay playback, the logicalDeviceCount and the AirPlay state determine the state
+        if (messagePayload.logicalDeviceCount > 0 && messagePayload.isProxyGroupPlayer && messagePayload.isAirplayActive) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
      * Tries to connect to an Apple TV.
      * @param forceReconnect Determines whether a reconnect should happen even if the Apple TV is already connected.
      * @param retryCount The number of retries that are left.
@@ -106,7 +125,7 @@ export class AppleTvClient extends EventEmitter {
 
                         // Updates the power state
                         if (m.payload.logicalDeviceCount === 0 || m.payload.logicalDeviceCount > 0) {
-                            this._isOn = m.payload.logicalDeviceCount > 0;
+                            this._isOn = this.getIsOn(m.payload);
                             this.emit('isOnChanged');
                         }
 
@@ -259,8 +278,8 @@ export class AppleTvClient extends EventEmitter {
         }
 
         // Returns the value indicating whether the device is on.
-        this.platform.logger.info(`[${this.name}] Returning value ${message.payload.logicalDeviceCount > 0} for power state`);
-        return message.payload.logicalDeviceCount > 0;
+        this.platform.logger.info(`[${this.name}] Returning value ${this.getIsOn(message.payload)} for power state`);
+        return this.getIsOn(message.payload);
     }
 
     /**
