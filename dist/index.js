77deee65e33723b3f3204050650cef7135fb9efc
Jeroen Luitwieler <jeluitwi@microsoft.com>
2020-09-26 23:00:38 +0200

    Remove build folders and restore gitignore

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var homebridge_framework_1 = require("homebridge-framework");
-var platform_1 = require("./lib/platform");
-// Registers the platform at homebridge
-module.exports = homebridge_framework_1.Homebridge.register(new platform_1.Platform());
